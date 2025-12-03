e4eabce8c1595d399600669063293ff418c7cb38
Marcos Claudiano <mvclaudiano@gmail.com>
2025-12-03 12:05:31 -0300

    fix corrections

--- packages/markscode/src/session/prompt.ts
+++ packages/markscode/src/session/prompt.ts
@@ -1,7 +1,6 @@
 import path from "path"
 import os from "os"
 import fs from "fs/promises"
-import path from "path"
 import z from "zod"
 import { Identifier } from "../id/id"
 import { MessageV2 } from "./message-v2"
