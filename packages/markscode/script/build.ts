54eb617436788dc73474900e0271ae5dd4402f0f
Marcos Claudiano <mvclaudiano@gmail.com>
2025-12-04 10:44:54 -0300

    Fix target for single build on linux

--- packages/markscode/script/build.ts
+++ packages/markscode/script/build.ts
@@ -79,7 +79,11 @@ const allTargets: {
 const targets = singleFlag
   ? allTargets
       .filter((item) => item.os === process.platform && item.arch === process.arch)
-      .map((item) => ({ ...item, avx2: false, target: `${item.os}-${item.arch}` }))
+      .map((item) => ({
+        ...item,
+        avx2: false,
+        target: process.platform === "linux" ? "linux-x64" : `${item.os}-${item.arch}`,
+      }))
   : allTargets
 
 await $`rm -rf dist || true`
